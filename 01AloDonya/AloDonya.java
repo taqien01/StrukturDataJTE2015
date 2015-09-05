068132f40b16bc925014baa9d279d87908e21ad3
Reza Muttaqien <taqien01@gmail.com>
2015-09-05 17:07:27 +0700

    Tugas Praktikum pertama : '01AloDonya'

---
+++ 01AloDonya/AloDonya.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class AloDonya here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AloDonya
+{
+   public static void main (String[] args){
+       System.out.println("Alo Donya");
+    }
+}
